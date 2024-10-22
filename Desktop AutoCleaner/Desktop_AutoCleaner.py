37cf334ca881f449b8cf26f3e24574205e36124c
Nick Rosentreter <nrs@orgadata.ag>
2024-10-22 11:33:38 +0200

    Import Libarys, i could use and Define the file types

--- Desktop AutoCleaner/Desktop_AutoCleaner.py
+++ Desktop AutoCleaner/Desktop_AutoCleaner.py
@@ -1,1 +1,15 @@
-print("Hello world")
+import os, shutil, tkinter as tk
+
+
+FILE_TYPES = {
+    "Bilder": [".jpeg", ".jpg", ".png", ".gif"],
+    "Dokumente": [".pdf", ".docx", ".txt"],
+    "Videos": [".mp4", ".mov"],
+    "Audio": [".mp3", ".wav"],
+    "Tabelle": [".xlsm", ".xlsx", ".csv"],
+    "Praesentation": [".pptx", ".odp"],
+    "Zip-Datein": [".zip"],
+    "Others": []
+}
+
+
