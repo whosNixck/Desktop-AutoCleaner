9469d41cdf31a6c01b896bc5df0adf09846c90ef
Nick Rosentreter <nrs@orgadata.ag>
2024-10-22 11:39:07 +0200

    Finished the Programm for now

--- Desktop AutoCleaner/Desktop_AutoCleaner.py
+++ Desktop AutoCleaner/Desktop_AutoCleaner.py
@@ -1,5 +1,5 @@
 import os, shutil, tkinter as tk
-
+from tkinter import filedialog
 
 FILE_TYPES = {
     "Bilder": [".jpeg", ".jpg", ".png", ".gif"],
@@ -12,4 +12,16 @@ FILE_TYPES = {
     "Others": []
 }
 
+def clean_folder(folder):
+    for file in os.listdir(folder):
+        ext = os.path.splitext(file)[1].lower()
+        dest_folder = next((f for f, exts in FILE_TYPES.items() if ext in exts), "Others")
+        os.makedirs(os.path.join(folder, dest_folder), exist_ok = True)
+        shutil.move(os.path.join(folder, file), os.path.join(folder, dest_folder, file))
 
+def select_folder():
+    root = tk.Tk(); root.withdraw()
+    return filedialog.askdirectory()
+
+folder = select_folder()
+if folder: clean_folder(folder)
